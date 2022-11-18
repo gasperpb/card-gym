81ee1763787cd015cca2a49234d6b7068b2e04ef
gasperpb <achillespb@hotmail.com>
2022-11-17 22:10:01 -0300

    imagens

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "MacBook",
+      price: 1400,
+      image: "https://picsum.photos/id/180/2400/1600",
+    },
+    {
+      id: "2",
+      name: "Old Car",
+      price: 2400,
+      image: "supino-reto-com-halteres.webp",
+    },
+    {
+      id: "3",
+      name: "W Shoes",
+      price: 1000,
+      image: "peitoral-superior.webp",
+    },
+    {
+      id: "4",
+      name: "Old Car",
+      price: 2400,
+      image: "supino-reto-com-halteres.webp",
+    },
+    {
+      id: "5",
+      name: "W Shoes",
+      price: 1000,
+      image: "peitoral-superior.webp",
+    },
+  ],
+};
+export default data;
